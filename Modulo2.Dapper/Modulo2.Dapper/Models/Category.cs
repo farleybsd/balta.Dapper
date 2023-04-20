984c2c9404e0d2dd9f753fac4268e3d6d57fdeda
TR031551 <farley.t.i@hotmail.com>
2023-04-20 19:58:36 -0300

    Dapper Modulo 2

---
+++ Modulo2.Dapper/Modulo2.Dapper/Models/Category.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modulo2.Dapper.Models
+{
+    internal class Category
+    {
+        // Tem que manter o mesmo Tipo e o nome da coluna e a Ordem Do Sql
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Url { get; set; }
+        public string Summary { get; set; }
+        public int Order { get; set; }
+        public string Description { get; set; }
+        public bool Featured { get; set; }
+    }
+}
